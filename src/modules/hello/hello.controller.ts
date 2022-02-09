5387e3b7c7de57d0b3da1ccddc6a8dae345230c6
Ming <1806191732@qq.com>
2022-02-09 17:21:12 +0800

    new(Swagger): 使用Swagger自动生成API文档

--- src/modules/hello/hello.controller.ts
+++ src/modules/hello/hello.controller.ts
@@ -11,30 +11,44 @@ import {
   Param,
 } from '@nestjs/common';
 import { HelloService } from './hello.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('/hello')
+@ApiTags('/hello')
 export class HelloController {
   // 使用constructor注入到一个对象中
   constructor(private readonly helloService: HelloService) {}
 
   // Get请求的处理
   @Get()
+  @ApiOperation({
+    summary: 'Get测试接口',
+  })
   fetch(@Query() { id }, @Headers('token') token): string {
     console.log('token==>', token);
     return this.helloService.getHelloById(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Post测试接口',
+  })
   save(@Body() { msg }): string {
     return this.helloService.save(msg);
   }
 
   @Delete()
+  @ApiOperation({
+    summary: 'Delete测试接口',
+  })
   delete(@Query() { id }): string {
     return this.helloService.remove(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Patch测试接口',
+  })
   update(@Param() { id }, @Body() { msg }): string {
     return this.helloService.update(id, msg);
   }
